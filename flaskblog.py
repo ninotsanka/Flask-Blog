a9bdf2d99956a1b836f34ed17bd6b5d4d6ffaa92
Nino Tsankashvili <ninotsankashvili@gmail.com>
2021-02-09 19:41:28 -0500

    added bootstrap

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Nino',
+        'title': 'Blog post 1',
+        'content': 'first post content here',
+        'date': '2,9,2021'
+    },
+    {
+        'author': 'Nino',
+        'title': 'Blog post 2',
+        'content': 'second post content here',
+        'date': '2,9,2021'
+    }
+]
 
 @app.route('/')
 @app.route('/home')
 def home_page():
-    return '<h1>Home Page!<h1>'
+    return render_template('home.html', posts=posts)
+    
 
 @app.route('/about')
 def about_page():
-    return '<h1>About Page!<h1>'
+    return render_template('about.html', title='About')
 
 
 if __name__ == '__main__':
